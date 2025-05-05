fa38d8d3238ee5987793f3a363290ed5ee76c93a
Vikesh115 <vikeshraut952@gmail.com>
2025-05-05 13:35:07 +0530

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -96,15 +96,6 @@ export default function Login() {
                                 Remember me
                             </label>
                         </div>
-
-                        <div className="text-sm">
-                            <Link
-                                to="/forgot-password"
-                                className="font-medium text-blue-600 hover:text-blue-500"
-                            >
-                                Forgot your password?
-                            </Link>
-                        </div>
                     </div>
 
                     <div>
